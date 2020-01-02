2a71a0b7112ad162c78403926735e1cde991df83
tama <tama@ttk1.net>
2020-01-03 02:54:00 +0900

    一旦保存

---
+++ src/Sudoku.ts
@@ -0,0 +1,98 @@
+export class Sudoku {
+  private field: number[][];
+
+  constructor() {
+    this.reset();
+  }
+
+  public set(i: number, j: number, value: number): boolean {
+    const tmp = this.field[i][j];
+    this.field[i][j] = value;
+    if (this.check(i, j)) {
+      return true;
+    } else {
+      this.field[i][j] = tmp;
+      return false;
+    }
+  }
+
+  public get(i: number, j: number) {
+    return this.field[i][j];
+  }
+
+  public reset() {
+    this.field = [];
+    for (let i = 0; i < 9; i++) {
+      this.field[i] = [];
+      for (let j = 0; j < 9; j++) {
+        this.field[i][j] = null;
+      }
+    }
+  }
+
+  private check(i: number, j: number): boolean {
+    // 行のチェック
+    const s = new Set();
+    for (let k = 0; k < 9; k++) {
+      const value = this.field[i][k];
+      if (value == null) {
+        continue;
+      } else if (s.has(value)) {
+        return false;
+      } else {
+        s.add(value);
+      }
+    }
+
+    // 列のチェック
+    s.clear();
+    for (let k = 0; k < 9; k++) {
+      const value = this.field[k][j];
+      if (value == null) {
+        continue;
+      } else if (s.has(value)) {
+        return false;
+      } else {
+        s.add(value);
+      }
+    }
+
+    // グループのチェック
+    s.clear();
+    i -= i % 3;
+    j -= j % 3;
+    for (let k = 0; k < 9; k++) {
+      const value = this.field[i + Math.floor(k / 3)][j + k % 3];
+      if (value == null) {
+        continue;
+      } else if (s.has(value)) {
+        return false;
+      } else {
+        s.add(value);
+      }
+    }
+    return true;
+  }
+
+  /*
+  public solve() {
+  }
+
+  public load(path: string) {
+  }
+  */
+
+  public print() {
+    for (let i = 0; i < 9; i++) {
+      let line = '';
+      for (let j = 0; j < 9; j++) {
+        if (this.field[i][j] == null) {
+          line += '□ ';
+        } else {
+          line += String(this.field[i][j]) + ' ';
+        }
+      }
+      console.log(line);
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log('hello, world');
+import { Sudoku } from './Sudoku';
+
+const sudoku = new Sudoku();
+
+console.log(sudoku.set(6, 7, 8));
+console.log(sudoku.set(7, 8, 8));
+console.log(sudoku.set(8, 7, 6));
+
+sudoku.print();
