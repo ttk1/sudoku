36f3eabaadfac0c185ef1f8224a0e1419dd95e83
tama <tama@ttk1.net>
2020-01-03 23:06:26 +0900

    数独を解く処理を実装

--- src/Sudoku.ts
+++ src/Sudoku.ts
@@ -5,7 +5,7 @@ export class Sudoku {
     this.reset();
   }
 
-  public set(i: number, j: number, value: number): boolean {
+  public set(i: number, j: number, value: number) {
     if (this.check(i, j, value)) {
       this.field[i][j] = value;
       return true;
@@ -28,33 +28,30 @@ export class Sudoku {
     }
   }
 
-  private check(i: number, j: number, value: number): boolean {
-    // 列
+  private check(i: number, j: number, value: number) {
     for (let k = 0; k < 9; k++) {
       if (k === i) {
         continue;
       }
-      if (this.field[k][j] === value) {
+      if (this.get(k, j) === value) {
         return false;
       }
     }
 
-    // 行
     for (let k = 0; k < 9; k++) {
       if (k === j) {
         continue;
       }
-      if (this.field[i][k] === value) {
+      if (this.get(i, k) === value) {
         return false;
       }
     }
 
-    // block
     for (let k = 0; k < 9; k++) {
       if (Math.floor(k / 3) === i % 3 && k % 3 === j % 3) {
         continue;
       }
-      if (this.field[i - i % 3 + Math.floor(k / 3)][j - j % 3 + k % 3] === value) {
+      if (this.get(i - i % 3 + Math.floor(k / 3), j - j % 3 + k % 3) === value) {
         return false;
       }
     }
@@ -62,10 +59,81 @@ export class Sudoku {
     return true;
   }
 
-  /*
-  public solve() {
+  private getCandidates(i: number, j: number) {
+    const s = new Set();
+    for (let k = 0; k < 9; k++) {
+      if (k === i) {
+        continue;
+      }
+      s.add(this.get(k, j));
+    }
+
+    for (let k = 0; k < 9; k++) {
+      if (k === j) {
+        continue;
+      }
+      s.add(this.get(i, k));
+    }
+
+    for (let k = 0; k < 9; k++) {
+      if (Math.floor(k / 3) === i % 3 && k % 3 === j % 3) {
+        continue;
+      }
+      s.add(this.get(i - i % 3 + Math.floor(k / 3), j - j % 3 + k % 3));
+    }
+
+    const candidates: number[] = [];
+    for (let k = 1; k < 10; k++) {
+      if (!s.has(k)) {
+        candidates.push(k);
+      }
+    }
+    return candidates;
   }
 
+  public solve() {
+    const f = () => {
+      const c: {i: number; j: number; candidates: number[]}[] = [];
+      for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+          if (this.get(i, j) == null) {
+            c.push({
+              i: i,
+              j: j,
+              candidates: this.getCandidates(i, j)
+            });
+          }
+        }
+      }
+
+      // cの長さが0なら、解けた
+      if (c.length === 0) {
+        return true;
+      }
+
+      // 候補の数が少ない順にソート
+      c.sort((a, b) => {
+        return a.candidates.length - b.candidates.length;
+      });
+
+      // 候補を順に試す
+      const i = c[0].i;
+      const j = c[0].j;
+      const tmp = this.get(i, j);
+      for (const value of c[0].candidates) {
+        if (this.set(i, j, value) && f()) {
+          return true;
+        }
+      }
+      this.set(i, j, tmp);
+      return false;
+    };
+    return f();
+  }
+
+  /*
+  public lock() {
+  }
   public load(path: string) {
   }
   */
@@ -82,10 +150,10 @@ export class Sudoku {
         if (j === 3 || j === 6) {
           line += ' │';
         }
-        if (this.field[i][j] == null) {
+        if (this.get(i, j) == null) {
           line += ' -';
         } else {
-          line += ' ' + String(this.field[i][j]);
+          line += ' ' + String(this.get(i, j));
         }
       }
       line += ' │';
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { Sudoku } from './Sudoku';
 const sudoku = new Sudoku();
 
 console.log(sudoku.set(6, 7, 8));
-console.log(sudoku.set(7, 8, 8));
+console.log(sudoku.set(7, 8, 8)); // -> 値のセット失敗
 console.log(sudoku.set(8, 7, 6));
+sudoku.print();
 
+sudoku.solve();
 sudoku.print();
